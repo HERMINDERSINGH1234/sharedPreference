59bb1bd4b705bea62fecaa15f0445a0a48b5014c
HERMINDER SINGH <hermindersingh12345@gmail.com>
2019-06-17 22:18:13 +0530

    Initial commit

---
+++ app/src/main/java/com/example/sunshine/sharedpreference/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.sunshine.sharedpreference
+
+import android.content.Context
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.edit
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val prefs = getPreferences(Context.MODE_PRIVATE)
+
+        var appOpenCount=0
+        appOpenCount=prefs.getInt("Count",0)
+        appOpenCount++
+        prefs.edit{
+            putInt("Count",appOpenCount)
+        }
+        textView.text=appOpenCount.toString()
+
+
+        saveBtn.setOnClickListener {
+
+            //prefs.edit().putString("Name", editText.text.toString()).apply()
+
+            prefs.edit {
+                putString("Name", editText.text.toString())
+
+            }
+
+        }
+
+        restoreBtn.setOnClickListener {
+            editText.setText(prefs.getString("Name", ""))
+        }
+
+
+    }
+}
